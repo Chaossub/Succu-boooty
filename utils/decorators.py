3e84cdce742422f10a1a36a4997be26ba8fefe67
Chaossub <jinxedprincess182@gmail.com>
2025-07-09 10:50:26 -0700

    Update decorators.py

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,12 +1,20 @@
+# utils/decorators.py
+from pyrogram import filters
 from pyrogram.types import Message
-from pyrogram.enums import ChatMemberStatus
 from functools import wraps
+
+SUPER_ADMIN_ID = 6964994611
 
 def admin_only(func):
     @wraps(func)
-    async def wrapper(client, message: Message):
-        member = await client.get_chat_member(message.chat.id, message.from_user.id)
-        if member.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
-            return await message.reply("🚫 You must be an admin to use this command.")
-        return await func(client, message)
+    async def wrapper(client, message: Message, *args, **kwargs):
+        user = message.from_user
+        if not user:
+            return
+        is_admin = (user.id == SUPER_ADMIN_ID) or \
+            (await client.get_chat_member(message.chat.id, user.id)).status in ("administrator", "creator")
+        if not is_admin:
+            return await message.reply("❌ You’re not allowed to use this.")
+        return await func(client, message, *args, **kwargs)
     return wrapper
+
