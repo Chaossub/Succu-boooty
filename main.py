6287b76fedf472a45b695e3b8e4232a21b07c1a8
Chaossub <jinxedprincess182@gmail.com>
2025-07-09 11:51:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import logging
 import pkgutil
 import importlib
+import inspect
 from pyrogram import Client
 from apscheduler.schedulers.background import BackgroundScheduler
 from pytz import timezone
@@ -17,25 +18,24 @@ logger = logging.getLogger(__name__)
 API_ID = int(os.environ["API_ID"])
 API_HASH = os.environ["API_HASH"]
 BOT_TOKEN = os.environ["BOT_TOKEN"]
-MONGO_URI = os.environ["MONGO_URI"]
-MONGO_DB = os.environ.get("MONGO_DB_NAME") or os.environ.get("MONGO_DBNAME")
 SCHED_TZ = timezone("America/Los_Angeles")
 
-# Init bot
+# Init
 app = Client("SuccuBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
-# Scheduler
 scheduler = BackgroundScheduler(timezone=SCHED_TZ)
 scheduler.start()
 logger.info("⏰ Scheduler started.")
 
-# Register all handlers
 def register_all_handlers(app):
     for _, module_name, _ in pkgutil.iter_modules(["handlers"]):
         module = importlib.import_module(f"handlers.{module_name}")
         if hasattr(module, "register"):
-            module.register(app, scheduler)
-        logger.info(f"✅ Registered handler: handlers.{module_name}")
+            args = inspect.signature(module.register).parameters
+            if len(args) == 2:
+                module.register(app, scheduler)
+            else:
+                module.register(app)
+            logger.info(f"✅ Registered handler: handlers.{module_name}")
     logger.info("✅ All handlers registered. Starting bot...")
 
 register_all_handlers(app)
