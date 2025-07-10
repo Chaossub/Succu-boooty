f35e2b9c22ee08be12a1635bcb97005c62abb9cc
Chaossub <jinxedprincess182@gmail.com>
2025-07-09 22:04:42 -0700

    Update check_admin.py

--- utils/check_admin.py
+++ utils/check_admin.py
@@ -1,5 +1,8 @@
 from pyrogram import Client
 
 async def is_admin(client: Client, chat_id: int, user_id: int) -> bool:
-    member = await client.get_chat_member(chat_id, user_id)
-    return member.status in ("administrator", "creator")
+    try:
+        member = await client.get_chat_member(chat_id, user_id)
+        return member.status in ("administrator", "creator")
+    except Exception:
+        return False
