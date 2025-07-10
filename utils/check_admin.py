135213098a4acedbec2ec0afd8e6b5f69d364e41
Chaossub <jinxedprincess182@gmail.com>
2025-07-09 21:13:35 -0700

    Update check_admin.py

--- utils/check_admin.py
+++ utils/check_admin.py
@@ -1,14 +1,9 @@
 from pyrogram import Client
-
-# Replace with your actual Telegram user ID
-SUPER_ADMIN_ID = 6964994611
+from pyrogram.types import ChatMember
 
 async def is_admin(client: Client, chat_id: int, user_id: int) -> bool:
-    # Always treat the super admin as admin
-    if user_id == SUPER_ADMIN_ID:
-        return True
     try:
-        member = await client.get_chat_member(chat_id, user_id)
+        member: ChatMember = await client.get_chat_member(chat_id, user_id)
         return member.status in ("administrator", "creator")
     except:
         return False
