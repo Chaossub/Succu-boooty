c7944ee1132a54135414df574c0677fee0d856ba
Chaossub <jinxedprincess182@gmail.com>
2025-07-09 20:27:22 -0700

    Update groups.py

--- utils/groups.py
+++ utils/groups.py
@@ -1,8 +1,7 @@
 import os
 
 GROUP_SHORTCUTS = {
-    "SUCCUBUS_SANCTUARY": int(os.getenv("SUCCUBUS_SANCTUARY", "0")),
-    "MODELS_CHAT": int(os.getenv("MODELS_CHAT", "0")),
-    "TEST_GROUP": int(os.getenv("TEST_GROUP", "0")),
+    "SUCCUBUS_SANCTUARY": int(os.getenv("SUCCUBUS_SANCTUARY")),
+    "MODELS_CHAT": int(os.getenv("MODELS_CHAT")),
+    "TEST_GROUP": int(os.getenv("TEST_GROUP")),
 }
-
