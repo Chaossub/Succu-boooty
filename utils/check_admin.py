ce2e5784f936fa3cb461f142dc2b717e212227ea
Chaossub <jinxedprincess182@gmail.com>
2025-07-10 10:58:42 -0700

    Update check_admin.py

--- utils/check_admin.py
+++ utils/check_admin.py
@@ -4,5 +4,6 @@ async def is_admin(client: Client, chat_id: int, user_id: int) -> bool:
     try:
         member = await client.get_chat_member(chat_id, user_id)
         return member.status in ("administrator", "creator")
-    except Exception:
+    except Exception as e:
+        print(f"[AdminCheck Error] {e}")
         return False
