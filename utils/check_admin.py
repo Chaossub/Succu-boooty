b511e950ecf9bf14c97a78d4eb79d784f77d0bf2
Chaossub <jinxedprincess182@gmail.com>
2025-07-09 21:02:55 -0700

    Update check_admin.py

--- utils/check_admin.py
+++ utils/check_admin.py
@@ -1,10 +1,7 @@
-SUPER_ADMIN_ID = 6964994611
+from pyrogram import Client
 
-async def is_admin(client, chat_id: int, user_id: int) -> bool:
-    if user_id == SUPER_ADMIN_ID:
-        return True
-    try:
-        member = await client.get_chat_member(chat_id, user_id)
-        return member.status in ("administrator", "creator")
-    except:
-        return False
+async def is_admin(client: Client, chat_id: int, user_id: int) -> bool:
+    async for member in client.get_chat_members(chat_id):
+        if member.user.id == user_id and member.status in ("administrator", "creator"):
+            return True
+    return False
