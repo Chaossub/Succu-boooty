7183a6d4919eb0995e0c721ff59dca0626d29fba
Chaossub <jinxedprincess182@gmail.com>
2025-07-11 21:11:11 -0700

    Update thread_id.py

--- handlers/thread_id.py
+++ handlers/thread_id.py
@@ -1,21 +1,22 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-
 def register(app: Client):
-    @app.on_message(filters.command("threadid") & filters.group)
+    @app.on_message(filters.command("threadid"))  # listen in any chat
     async def threadid_handler(client: Client, message: Message):
-        # Direct thread ID of this message (if sent inside a forum topic)
-        tid = getattr(message, "message_thread_id", None)
-        # If user replied to another message, show its thread ID too
-        reply_tid = None
+        # ID of this message
+        msg_id = message.id
+        # Thread ID if in a forum topic
+        thread_id = getattr(message, "message_thread_id", None)
+        # If replying, thread of original
+        reply_thread = None
         if message.reply_to_message:
-            reply_tid = getattr(message.reply_to_message, "message_thread_id", None)
-        
+            reply_thread = getattr(message.reply_to_message, "message_thread_id", None)
+
         await message.reply_text(
             f"<b>chat_id:</b> <code>{message.chat.id}</code>\n"
-            f"<b>msg_id:</b> <code>{message.message_id}</code>\n"
-            f"<b>thread_id:</b> <code>{tid}</code>\n"
-            f"<b>reply_thread:</b> <code>{reply_tid}</code>",
+            f"<b>msg_id:</b> <code>{msg_id}</code>\n"
+            f"<b>thread_id:</b> <code>{thread_id}</code>\n"
+            f"<b>reply_thread:</b> <code>{reply_thread}</code>",
             quote=True
         )
