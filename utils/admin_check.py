d459e3d69db9dc1eb2b44174fa163ad71c393139
Chaossub <jinxedprincess182@gmail.com>
2025-07-15 18:48:45 -0700

    Update admin_check.py

--- utils/admin_check.py
+++ utils/admin_check.py
@@ -1,8 +1,11 @@
-from pyrogram.enums import ChatMemberStatus
+import os
+from pyrogram.errors import UserNotParticipant, ChatAdminRequired
 
 async def is_admin(client, chat_id, user_id):
+    if str(user_id) == str(os.getenv("OWNER_ID")):
+        return True
     try:
         member = await client.get_chat_member(chat_id, user_id)
-        return member.status in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]
-    except Exception:
+        return member.status in ("administrator", "creator")
+    except (UserNotParticipant, ChatAdminRequired):
         return False
