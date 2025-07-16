2b478aedef09673e118b9cdd845b0546591fe549
Chaossub <jinxedprincess182@gmail.com>
2025-07-15 20:56:54 -0700

    Delete handlers/get_id.py

--- handlers/get_id.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import filters, Client
-from pyrogram.types import Message
-
-def register(app: Client):
-    @app.on_message(filters.command("getid"))
-    async def get_id(_, message: Message):
-        await message.reply_text(f"Your ID: `{message.from_user.id}`\nChat ID: `{message.chat.id}`", parse_mode="markdown")
