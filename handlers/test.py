0102550f57d5f4fb365a1daa25a142cb712be2a1
Chaossub <jinxedprincess182@gmail.com>
2025-07-15 20:56:37 -0700

    Delete handlers/test.py

--- handlers/test.py
+++
@@ -1,8 +0,0 @@
-# handlers/test.py
-
-from pyrogram import Client, filters
-
-def register(app: Client):
-    @app.on_message(filters.command("ping"))
-    async def ping_handler(client, message):
-        await message.reply("🏓 Pong!")
